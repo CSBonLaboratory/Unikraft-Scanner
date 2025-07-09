fedc028b4769cd6b02ce9aebd574dcba507e5fa2
CSBonLaboratory <csbon420@protonmail.com>
2025-07-09 21:16:19 +0300

    Split tests for compilation block discovery stage and incomplete tests for compilation line parsing. Remove prepare.sh dependency and integrated it as native code

--- src/UnikraftScanner/UnikraftScanner.Tests/Symbols/dependencies/prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ../../UnikraftScanner.Client/Symbols
-make
-cp ./bin/*.so ../../UnikraftScanner.Tests/Symbols/dependencies/TestPluginBlockFinder.so
-# go back to where PrePareSymbolMain.cs is
-cd -
