6655a0ec75b17dd5f7c904a47fbc70416df3f928
CSBonLaboratory <csbon420@protonmail.com>
2025-08-15 17:44:30 +0300

    Add prototype for compilation command parsing. Unlikely to continue this

---
+++ src/UnikraftScanner/UnikraftScanner.Client/Symbols/CompileParse/CFamilyException.cs
@@ -0,0 +1,18 @@
+﻿namespace UnikraftScanner.Client;
+
+public class CFamilyException : InvalidOperationException
+{
+    public CFamilyException()
+    {
+    }
+
+    public CFamilyException(string message)
+        : base(message)
+    {
+    }
+
+    public CFamilyException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
---
+++ src/UnikraftScanner/UnikraftScanner.Client/Symbols/CompileParse/UnknownCompilerFound.cs
@@ -0,0 +1,18 @@
+﻿namespace UnikraftScanner.Client;
+
+public class UnknownCompilerFound : InvalidOperationException
+{
+    public UnknownCompilerFound()
+    {
+    }
+
+    public UnknownCompilerFound(string message)
+        : base(message)
+    {
+    }
+
+    public UnknownCompilerFound(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
